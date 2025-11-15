54ed9e43a4e6828256901eb8ce52e87d725eaf55
MohdAseel <mohd.aseel.vkd@gmail.com>
2025-11-15 16:07:13 +0530

    Add initial project files including .gitignore, algorithms, and main notebook

---
+++ main.py
@@ -0,0 +1,10 @@
+# import pandas as pd
+# import numpy as np
+
+
+# TRAIN_DATA_PATH = 'MNIST_train.csv'
+# VAL_DATA_PATH = 'MNIST_validation.csv'
+
+# train_data = pd.read_csv(TRAIN_DATA_PATH)
+# val_data = pd.read_csv(VAL_DATA_PATH)
+
